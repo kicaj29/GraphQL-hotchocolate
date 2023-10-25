acd00a0bb3c92e5e284b44998a5ecfbe4db41d90
Jacek Kowalski <Jacek.Kowalski@hyland.com>
2023-10-25 10:49:00 +0200

    SchemaLoader

---
+++ SchemaLoader/SchemaLoader/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HotChocolate.Language;
+using HotChocolate.Utilities.Introspection;
+
+Console.WriteLine("Hello, World!");
+
+
+HttpClient httpClient = new HttpClient();
+httpClient.BaseAddress = new Uri("https://.../graphql");
+httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer",
+    "eyJh...");
+IntrospectionClient introspectionClient = new IntrospectionClient();
+DocumentNode schema = await introspectionClient.DownloadSchemaAsync(httpClient);
+string schemaString = schema.ToString();
+Console.WriteLine(schemaString);
+File.WriteAllText("gql-schema.schema", schemaString);
+
+
+
+Console.ReadKey();
